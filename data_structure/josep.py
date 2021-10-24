ca85a74c32f664d0ca86e52f0a5bc7bb81a06265
jang-hoon <wkdal904@naver.com>
2021-10-24 18:42:30 +0900

    1024

--- data_structure/josep.py
+++ data_structure/josep.py
@@ -1,18 +1,16 @@
-# Authored by : klm03025
-# Co-authored by : -
-# Link : http://boj.kr/1306fa8a1c5a4f4cab631f833d92636a
-import sys
-from collections import deque
-
-def input():
-    return sys.stdin.readline().rstrip()
-
 N, K = map(int, input().split())
-_list = []
-q = deque([i + 1 for i in range(N)])
 
-while len(q) != 0:
-    q.rotate(-K)
-    _list.append(q.pop())
+jose=[i for i in range(1, N+1)]
+#N크기의 리스트 생성
 
-print('<' + ', '.join(map(str, _list)) + '>')
+result=[]
+
+seqNo = K-1
+while len(jose):
+    if seqNo>=len(jose):
+        seqNo=seqNo%len(jose)
+    else:
+        result.append(str(jose.pop(seqNo)))
+        seqNo+=(K-1)
+
+print("<", ", ".join(result), ">", sep='')
